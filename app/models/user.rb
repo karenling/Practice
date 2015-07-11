bfd77d350c9a8e2bcd74494b96cd3cb58ba15447
Karen Ling <karensling@gmail.com>
2015-07-11 14:48:52 -0700

    Implement users.

---
+++ app/models/user.rb
@@ -0,0 +1,21 @@
+class User < ActiveRecord::Base
+  attr_accessor :password
+  validates :username, :session_token, :password_digest, presence: true
+  validates :password, length: { minimum: 6, allow_nil: true }
+  validates :username, :session_token, uniqueness: true
+  after_initialize :ensure_session_token
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def ensure_session_token
+    self.session_token ||= SecureRandom::urlsafe_base64
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+end
