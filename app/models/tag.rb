f987e46183eec9f1fd773dc290c43847999593ff
Karen Ling <karensling@gmail.com>
2015-07-11 16:14:05 -0700

    Create tag resource.

---
+++ app/models/tag.rb
@@ -0,0 +1,26 @@
+# == Schema Information
+#
+# Table name: tags
+#
+#  id         :integer          not null, primary key
+#  name       :string           not null
+#  created_at :datetime
+#  updated_at :datetime
+#
+
+class Tag < ActiveRecord::Base
+  validates :name, presence: true
+
+  has_many(
+    :taggings,
+    class_name: 'Tagging',
+    foreign_key: :tag_id,
+    primary_key: :id
+  )
+
+  has_many(
+    :posts,
+    through: :taggings,
+    source: :post
+  )
+end
