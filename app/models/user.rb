d6ebca8b805cefcd748b4bb497c9565f6d4e5844
Karen Ling <karensling@gmail.com>
2015-07-11 16:13:13 -0700

    Include annotate.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  username        :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime
+#  updated_at      :datetime
+#
+
 class User < ActiveRecord::Base
   attr_accessor :password
   validates :username, :session_token, :password_digest, presence: true
