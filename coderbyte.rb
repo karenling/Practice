a9ea0e4573f6ef89281059cf8fe2033426ec124e
Karen Ling <karensling@gmail.com>
2015-07-14 14:56:01 -0700

    Longest word.

--- coderbyte.rb
+++ coderbyte.rb
@@ -18,3 +18,17 @@ def FirstFactorial(num)
 
   total
 end
+
+# Using the Ruby language, have the function LongestWord(sen) take the sen parameter being passed and return the largest word in the string. If there are two or more words that are the same length, return the first word from the string with that length. Ignore punctuation and assume sen will not be empty.
+
+def LongestWord(sen)
+  longest = ""
+
+  sen.split(" ").each do |word|
+    if word.length > longest.length
+      longest = word
+    end
+  end
+
+  longest
+end
